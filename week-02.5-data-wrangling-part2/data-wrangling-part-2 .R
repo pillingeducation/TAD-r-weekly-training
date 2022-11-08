0c249877100dec5740f5502605b1b93e73ee17fd
PILLING <Samuel.PILLING@EDUCATION.GOV.UK>
2022-11-08 11:32:08 +0000

    added qmd presenation and created question in the README.rmd file

--- week-02.5-data-wrangling-part2/data-wrangling-part-2 .R
+++ week-02.5-data-wrangling-part2/data-wrangling-part-2 .R
@@ -1,8 +1,6 @@
 library(nycflights13)
 library(tidyverse)
 
-install.packages("nycflights13")
-
 
 flights_edit <-flights %>% mutate( long_haul = if_else(air_time > (60*7),1,0))
 
@@ -21,5 +19,24 @@ popular_dests <- flights %>%
   summarise(count_flights = n()) %>%
   filter(origin == "JFK")
 
+ 
+ flight_delay <- flights %>% mutate(total_delay = dep_delay + arr_delay)
+ 
+ flight_delay %>% mutate(refund = if_else(total_delay < -30, 1, 0))
+ 
+ flight_delay %>% mutate(refund_groups = case_when (total_delay < -60 ~ 1,
+                                                    total_delay < -30 ~ 2,
+                                                    total_delay < 0 ~ 3,
+                                                    TRUE ~ 0 ,)) %>% count(refund_groups)
 
- flights %>%  pivot_longer()
+ 
+ flights %>% group_by(dest) %>%  summarise(delayed_flights = sum(if_else(dep_delay < 0,1,0), na.rm = T))
+ 
+ 
+ 
+ flights %>%
+   left_join(airports %>% select(faa,name), by = c("dest" = "faa")) %>%
+   rename(dest_airport = name)
+a <- flights %>%
+  left_join(airports %>% select(faa,name), by = c("dest" = "faa")) %>%
+  rename(dest_airport = name) 
